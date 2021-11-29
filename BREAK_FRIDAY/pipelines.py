fec55b2cfc74aa337cc1ea15c976f9b5ef2c69b5
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-11-29 17:59:40 -0300

    feat: fix pipelines

--- BREAK_FRIDAY/pipelines.py
+++ BREAK_FRIDAY/pipelines.py
@@ -13,7 +13,23 @@ from datetime import datetime
 class BreakFridayPipeline:
     def open_spider(self, spider):
         current_time = datetime.now().strftime("%H-%M-%S")
-        self.file = open(f"results_break-{current_time}.txt", "w")
+        print("\n\n\nOPENING PACKAGES PIPELINE\n\n\n")
+        self.file = open(f"results_break_packages-{current_time}.txt", "w")
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        print("Pipeline :", item)
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
+
+class HotelsPipeline:
+    def open_spider(self, spider):
+        current_time = datetime.now().strftime("%H-%M-%S")
+        print("\n\n\nOPENING HOTELS PIPELINE\n\n\n")
+        self.file = open(f"results_break_hotels-{current_time}.txt", "w")
 
     def close_spider(self, spider):
         self.file.close()
