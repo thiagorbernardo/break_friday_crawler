295e261823e274527bb30c44adc368c81fc3cb5d
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-11-29 18:01:13 -0300

    feat: spiders running brasil

--- BREAK_FRIDAY/spiders/Hotels.py
+++ BREAK_FRIDAY/spiders/Hotels.py
@@ -4,34 +4,32 @@ import random
 
 from BREAK_FRIDAY.items import BreakFridayBanner
 
-class HurbSpider(scrapy.Spider):
-    name = "Hurb"
+class HotelsSpider(scrapy.Spider):
+    name = 'Hotels'
     allowed_domains = ['hurb.com']
     start_urls = []
-    # start_urls = ['https://www.hurb.com/br']
-    # allowed_domains = ["http://127.0.0.1:5500"]
-    # start_urls = ["http://127.0.0.1:5500/hurb.html"]
 
     def __init__(self):
-        print("\nINIT PACKAGES\n")
+        print("\nINIT HOTELS\n")
         start_urls = []
         countries = [("pt_br", "brasil")]
         # countries = countries_for_language("pt_br")
         random.shuffle(countries)
         for country in countries:
             options = [
-                f"https://www.hurb.com/br/search/packages?q={country[1]}&tab=packages&page={i}"
-                for i in range(1, 5)
+                f"https://www.hurb.com/br/search/hotels?q={country[1]}&tab=hotels&rooms=2&page={i}"
+                for i in range(1, 2)
             ]
             start_urls.extend(options)
         self.start_urls = start_urls
 
     def parse(self, response):
-        print("PACKAGES PAGE: ", response.url)
+        print("HOTELS PAGE: ", response.url)
         options = []
         for div in response.css("a"):
-            package = div.css(".hrc-1_8b9").getall()
-            if package:
+            hotel = div.css(".hrc-1_8b9").getall()
+            print(div.get())
+            if hotel:
                 url = div.css("a::attr(href)").extract_first()
                 options.append(url)
 
@@ -39,9 +37,9 @@ class HurbSpider(scrapy.Spider):
             yield response.follow(option, self.parse_banner)
 
     def parse_banner(self, response):
-        print("PACKAGE: ", response.url)
+        print("HOTEL: ", response.url)
         for div in response.css("div"):
             banner_break = div.css(".banner-break").getall()
             if banner_break:
-                print("\n\n\nFOUND BANNER PACKAGE: ", banner_break, "\n\n\n")
+                print("\n\n\nFOUND BANNER HOTEL: ", banner_break, "\n\n\n")
                 yield BreakFridayBanner(name=div.css(".banner-break").extract_first(), url=response.url)
